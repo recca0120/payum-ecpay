7e80df32937b8c1b583ac02711f799d715c6dc45
Recca Tsai <recca0120@gmail.com>
2016-12-08 12:25:43 +0000

    Apply fixes from StyleCI

--- src/LogisticsApi.php
+++ src/LogisticsApi.php
@@ -2,13 +2,13 @@
 
 namespace PayumTW\Ecpay;
 
+use Http\Message\MessageFactory;
+use Payum\Core\HttpClientInterface;
 use PayumTW\Ecpay\Bridge\Ecpay\Device;
 use PayumTW\Ecpay\Bridge\Ecpay\IsCollection;
 use PayumTW\Ecpay\Bridge\Ecpay\LogisticsType;
-use PayumTW\Ecpay\Bridge\Ecpay\LogisticsSubType;
-use Http\Message\MessageFactory;
-use Payum\Core\HttpClientInterface;
 use PayumTW\Ecpay\Bridge\Ecpay\EcpayLogistics;
+use PayumTW\Ecpay\Bridge\Ecpay\LogisticsSubType;
 
 class LogisticsApi extends BaseApi
 {
